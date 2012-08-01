5f3471bdee4445b49d121477619ba0f74161e782
get --version <get -version>
2012-07-31 20:58:01 -0500

    Refactor para soportar hasta el 5. Cambio if por Hash

---
+++ features/step_definition/roman_steps.rb
@@ -0,0 +1,24 @@
+require 'rspec/expectations'
+
+Given /^un conversor$/ do
+  @conversor = Conversor.new
+end
+
+When /^le paso (\d+)$/ do |numero|
+  @resultado = @conversor.convertir_romano(numero.to_i)
+end
+
+Then /^lo convierte a ([A-Z]*)/ do |valor_romano|
+  @resultado.should == valor_romano
+end
+
+class Conversor
+	def convertir_romano(numero)
+		x = { 	1 => "I", 
+				2 => "II",
+				3 => "III",
+				4 => "IV",
+				5 => "V" }
+		return x.fetch( numero ) if x.has_key?( numero )  
+	end
+end
