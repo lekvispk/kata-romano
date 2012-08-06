3d3a92a6e5bdad11d5cd7cb26941d490e024d55e
Elvis Rubén Campos Mori <lekvispk@gmail.com>
2012-08-06 01:13:50 -0500

    numero 10

--- features/roman.feature
+++ features/roman.feature
@@ -19,3 +19,4 @@ Scenario Outline: Convertir <arabigo>
 	| 7		  | VII	    |
 	| 8		  | VIII	|
 	| 9		  | IX  	|
+	| 10	  | X 	 	|
--- features/step_definition/roman_steps.rb
+++ features/step_definition/roman_steps.rb
@@ -22,7 +22,8 @@ class Conversor
 				6 => "VI",
 				7 => "VII",
 				8 => "VIII",
-				9 => "IX" }
+				9 => "IX",
+				10 => "X"  }
 		return x.fetch( numero ) if x.has_key?( numero )  
 	end
 end
